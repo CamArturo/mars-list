dd9b332b6c340c66204cb0b3007799da8d125387
Camilo Snapp <camilo.snapp@gmail.com>
2018-07-16 10:55:30 -0600

    Add seeding

--- db/migrations/20180716102211_initial.js
+++ db/migrations/20180716102211_initial.js
@@ -1,8 +1,26 @@
-
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('items', function(table) {
+      table.increments('id').primary();
+      table.string('item_name');
+      table.boolean('item_packed');
+      table.timestamps(true, true);
+    })
+
+    // knex.schema.createTable('packed', function(table) {
+    //   table.increments('id').primary();
+    //   table.boolean('packed');
+    //   table.integer('item_id').unsigned();
+    //   table.foreign('item_id').references('items.id');
+    //
+    //   table.timestamps(true, true);
+    // })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    // knex.schema.dropTable('packed'),
+    knex.schema.dropTable('items')
+  ]);
 };
--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: "postgres://localhost/marsList",
+    connection: "postgres://localhost/marslist",
     migrations: {
       directory: './db/migrations'
     },
@@ -28,20 +28,20 @@ module.exports = {
   //   }
   // },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
